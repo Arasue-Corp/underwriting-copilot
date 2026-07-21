eadbe1f57a0fd41facee9fb83c8687e95a502da0
Josue Daniel <josuel.barreras@gmail.com>
2026-07-20 21:29:29 -0700

    fix: add maxAge to language cookie for persistence

--- web/src/app/actions/language.ts
+++ web/src/app/actions/language.ts
@@ -3,5 +3,5 @@ import { cookies } from 'next/headers'
 
 export async function setLanguageCookie(lang: 'en' | 'es') {
   const cookieStore = await cookies()
-  cookieStore.set('NEXT_LOCALE', lang, { path: '/' })
+  cookieStore.set('NEXT_LOCALE', lang, { path: '/', maxAge: 31536000 })
 }
